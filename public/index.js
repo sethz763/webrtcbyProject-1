38d0d2fa6751d7a2db70b44121aa27aa931629a4
sethz763 <sethz763@gmail.com>
2022-02-12 16:58:57 -0500

    sh: q: command not found

--- public/index.js
+++ public/index.js
@@ -1,2 +1,159 @@
-const socket = io('http://localhost:4200')
+const socket = io()
+const fromSocket = document.getElementById('userId')
+const heading = document.getElementById('heading')
+const localVideo = document.getElementById('localVideo')
+const remoteVideo = document.getElementById('remoteVideo')
+const call= document.getElementById('call')
+const stop= document.getElementById('stop')
+const mute= document.getElementById('mute')
+const unMute= document.getElementById('unMute')
+const toSocket = document.getElementById('toSocket')
+let tracks = []
+const configuaration = {iceServers:[{urls: 'stun:stun.l.google.com:19302'}]}
+let peer = new RTCPeerConnection(configuaration)
+let toSocketId, fromSocketId
 
+//Get socket Id
+
+socket.on('connect', () => {
+    fromSocket.innerHTML = socket.id
+    fromSocketId = socket.id
+})
+
+//get local media
+const openMediaDevices = async() =>{
+    try{
+        let stream = await navigator.mediaDevices.getUserMedia({video:true, audio:true})
+        localVideo.srcObject = stream
+        tracks = stream.getTracks()
+        return stream
+    }catch(error){
+        console.log(error)
+    }
+}
+
+//create offer
+const createOffer = async() => {
+    try {
+        let stream = await openMediaDevices()
+        stream.getTracks().forEach(track => peer.addTrack(track)) 
+        let offer = await peer.createOffer()
+        peer.setLocalDescription(new RTCSessionDescription(offer))
+        //ice candidate
+        peer.addEventListener('icecandidate', e => {
+            if(e.candidate){
+                console.log(e.candidate)
+                socket.emit('callerCandidate', {'candidate':e.candidate, 'fromSocketId':fromSocketId, 'toSocketId':toSocketId })
+            }
+        })
+        //send offer to server
+        toSocketId = toSocket.value
+        socket.emit('offer', {'offer':offer, 'fromSocketId':fromSocketId, 'toSocketId':toSocketId })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//create Answer
+const createAnswer = async(destination) => {
+    try{
+        let stream = await openMediaDevices()
+        stream.getTracks().forEach(track => peer.addTrack(track)) 
+        let answer = await peer.createAnswer()
+        peer.setLocalDescription(new RTCSessionDescription(answer))
+
+        //ice candidate
+        peer.addEventListener('icecandidate', e => {
+            if(e.candidate){
+                console.log(e.candidate)
+                socket.emit('calleeCandidate', {'candidate':e.candidate, 'destination':destination})
+            }
+        })
+        
+        //send answer to server
+        socket.emit('answer', {'answer': answer, 'destination':destination})
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+
+//receive offer
+socket.on('offer', data=>{
+    peer.setRemoteDescription(data.offer)
+    let stream = new MediaStream()
+    createAnswer(data.fromSocketId)
+    peer.ontrack = e => {
+        stream.addTrack(e.track)
+        remoteVideo.srcObject = stream
+        console.log(e)
+    }
+})
+
+//receive answer
+socket.on('answer', data => {
+    peer.setRemoteDescription(data.answer)
+    let stream = new MediaStream()
+    peer.ontrack = e => {
+        stream.addTrack(e.track)
+        remoteVideo.srcObject = stream
+        console.log(e)
+    }
+})
+
+//start a call
+call.addEventListener('click', () => {
+    createOffer()
+    mute.addEventListener('click', muteTracks)
+    stop.addEventListener('click', stopTracks)
+})
+
+//mute tracks
+const muteTracks = ()  => {
+    tracks.forEach( track => track.enabled = false)
+    unMute.addEventListener('click', unMuteTracks)
+}
+
+//
+const unMuteTracks = ()  => {
+    tracks.forEach( track => track.enabled = true)
+}
+
+//stop tracks
+const stopTracks = () => {
+    tracks.forEach( track => track.stop() )
+}
+
+//caller candidate
+socket.on('callerCandidate', data => {
+    peer.addIceCandidate(data)
+    console.log(data)
+})
+
+//callee candidate
+socket.on('calleeCandidate', data =>{
+    peer.addIceCandidate(data)
+    console.log(data)
+})
+
+
+const text = "WEBRTC TRAINING - "
+
+function addZero(i) {
+    if (i < 10) {i = "0" + i}
+    return i;
+  }
+
+function updateTime(){
+    let date_time = new Date()
+    let hour = date_time.getHours().toString()
+    let minute = addZero(date_time.getMinutes().toString())
+    let second = addZero(date_time.getSeconds().toString())
+    heading.textContent = text + hour + ":" + minute + ":" + second
+}
+
+setInterval(updateTime, 500);
+
+    
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require("http")
 const app = express()
 const httpServer = http.createServer(app)
 const port = process.env.PORT || 4200
-
 const socketio = require('socket.io')
 
 
@@ -13,7 +12,40 @@ httpServer.listen(port, () =>{
     console.log("server starting on port : ", port)
 })
 
+var ip = require("ip");
+console.dir ( ip.address() );
+
 const io = socketio(httpServer)
+
 io.on('connection', (socket) =>{
     console.log('user connected ', socket.id)
+
+    //receive/send offer
+    socket.on('offer', data => {
+        io.to(data.toSocketId).emit('offer', data)
+        //io.emit('offer', data)
+    })
+
+    //receive/send answer
+    socket.on('answer', data =>{
+        io.to(data.destination).emit('answer', data)
+    })
+
+    //caller ice candidates
+    socket.on('callerCandidate', data => {
+        io.to(data.toSocketId).emit('callerCandidate', data.candidate)
+    })
+
+    //callee candidate
+    socket.on('calleeCandidate', data => {
+        io.to(data.destination).emit('calleeCandidate', data.candidate)
+    })
+
 })
+
+
+
+
+
+
+
